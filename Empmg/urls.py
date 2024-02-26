de12cb1d72cae6a3a9fd439500729b7357aaed8a
Grayson Leaphart <graysonleaphart@Graysons-MacBook-Pro.local>
2024-02-25 19:32:45 -0500

    your messege

--- Empmg/urls.py
+++ Empmg/urls.py
@@ -3,4 +3,9 @@ from Empmg import views
 
 urlpatterns = [
     path('Home/', views.home, name='home'),
+    path('Signin/', views.signin, name='signin'),
+    path('CreateAcc/', views.createacc, name='createaccount'),
+    path('UserHome/', views.userhome, name='userhome'),
+    path('Updateemp/', views.updatepage, name='update'),
+    path('Createemp/', views.createemp, name="createemp")
 ]
--- Empmg/views.py
+++ Empmg/views.py
@@ -4,4 +4,22 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'home.html')
 
+def userhome(request):
+    return render(request, 'userhome.html')
 
+def updatepage(request):
+    return render(request, 'update.html')
+
+def createemp(request):
+    return render(request, 'createemp.html')
+
+def signin(request):
+    return render(request, 'signin.html')
+
+def createacc(request):
+    return render(request, 'createaccount.html')
+
+#future reference for when beginning redirecting pages between one another
+#def logout_view(request):
+  #  logout(request)
+    #return redirect('home')  
